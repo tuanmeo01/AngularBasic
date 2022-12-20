89715c1a9a1eb8e735a13e2002f43bcb659fe780
tuanmeo01 <truonghoang.th89@gmail.com>
2022-12-20 16:25:33 +0700

    data binding,event binding

--- Desktop/CMC/AngularBasic/src/app/app.component.ts
+++ Desktop/CMC/AngularBasic/src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'angular-test';
+  title = 'angular-basic';
+  user = {
+    name: 'tuan',
+    age: '21',
+  };
+  handleClick() {
+    console.log('clicked');
+  }
 }
--- Desktop/CMC/AngularBasic/src/app/app.module.ts
+++ Desktop/CMC/AngularBasic/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,15 +7,9 @@ import { AppComponent } from './app.component';
 import { HelloWorldComponent } from './hello-world/hello-world.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HelloWorldComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HelloWorldComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
